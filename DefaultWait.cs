1472908d216b7a73a45e0ea9e0ef167a472ad9c3
JustMeKatherine <kaskakar@gmail.com>
2017-01-02 02:39:17 +0100

    first commit

---
+++ DefaultWait.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using static OpenQA.Selenium.Support.UI.ExpectedConditions;
+
+namespace WebdriverTimeoutsTutorial
+{
+    [TestClass]
+    public class DefaultWait
+    {
+        private IWebDriver _driver;
+        private const string URI = "https://the-internet.herokuapp.com/dynamic_loading/2";
+        private By elementId = By.Id("finish");
+        private By elementXPath = By.XPath(".//*[@id='finish']");
+        Stopwatch _stopwatch = new Stopwatch();
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _driver = WebDriverCreator.RemoteInitialize();
+            //_driver = WebDriverCreator.BasicInitialize();
+            //go to a url that contains a dynamically loading page element
+            _driver.Navigate().GoToUrl(URI);
+            //click the start button
+            _driver.FindElement(By.TagName("button")).Click();
+        }
+
+        [TestCleanup]
+        public void Teardown()
+        {
+            _driver.Close();
+            _driver.Quit();
+        }
+
+        [TestMethod]
+        public void DefaultWaitTest()
+        {
+            //var wait = new DefaultWait<IWebDriver>(_driver)
+            //{
+            //    Timeout = TimeSpan.FromSeconds(10),
+            //    PollingInterval = TimeSpan.FromSeconds(1)
+            //};
+
+            IClock clock = new SystemClock();
+            var wait2 = new WebDriverWait(clock, _driver, TimeSpan.FromSeconds(15), TimeSpan.FromSeconds(1));
+            //_stopwatch.Start();
+            wait2.Until(ElementIsVisible(elementId));
+            //wait.Until(ElementIsVisible(elementXPath));
+            //_stopwatch.Stop();
+            Trace.WriteLine($"Time elapsed for element identification: {_stopwatch.Elapsed.TotalSeconds}s");
+        }
+    }
+}
---
+++ WebDriverCreator.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+using System;
+
+namespace WebdriverTimeoutsTutorial
+{
+    public static class WebDriverCreator
+    {
+        public static IWebDriver BasicInitialize()
+        {
+            return new ChromeDriver(@"C:\Users\Kat\Documents\Visual Studio 2015\Projects\4_Selenium_own\WebdriverTimeoutsTutorial\Drivers");
+            //return new FirefoxDriver(@"C:\Users\Kat\Documents\Visual Studio 2015\Projects\3_frameworki\WebdriverTimeoutsTutorial\Drivers\chromedriver.exe");
+        }
+
+        public static IWebDriver RemoteInitialize()
+        {
+            var capability = new DesiredCapabilities();
+            //capability.SetCapability("browserstack.user", Environment.GetEnvironmentVariable("BROWSERSTACK_USER", EnvironmentVariableTarget.User));
+            //capability.SetCapability("browserstack.key", Environment.GetEnvironmentVariable("BROWSERSTACK_KEY", EnvironmentVariableTarget.User));
+            capability.SetCapability("browserstack.user", "katarzynakarp1");
+            capability.SetCapability("browserstack.key", "ChKGEd5vbZsnkmXHgWHN");
+
+            capability.SetCapability("browser", "Edge");
+            capability.SetCapability("browser_version", "14.0");
+            capability.SetCapability("os", "Windows");
+            capability.SetCapability("os_version", "10");
+            capability.SetCapability("resolution", "1024x768");
+
+            capability.SetCapability("browserstack.debug", "true");
+
+            var driver = new RemoteWebDriver(new Uri("http://hub.browserstack.com/wd/hub/"), capability);
+            return driver;
+        }
+    }
+}
